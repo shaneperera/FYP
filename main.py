Fixed GPU issue with torch.no_grad() for validation Below average validation results

main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     study_data = get_study_data(study_type='XR_WRIST')
     # #### Create dataloaders pipeline
     data_cat = ['train', 'valid']  # data categories
-    dataloaders = get_dataloaders(study_data, batch_size=2)
+    dataloaders = get_dataloaders(study_data, batch_size=8)
     dataset_sizes = {x: len(study_data[x]) for x in data_cat}
 
     # #### Build model
@@ -47,6 +47,7 @@ if __name__ == '__main__':
 
     model = densenet169(pretrained=True)
     model = model.cuda()
+    #cudnn.benchmark = True
 
     criterion = Loss(Wt1, Wt0)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)

train.py
@@ -81,31 +81,49 @@ def train_model(model, criterion, optimizer, dataloaders, scheduler,
                     # Convert the label (0 or 1) to an integer Tensor
                     labels = data[1][j].type(torch.Tensor)
 
+                    if phase == 'valid':
+                        with torch.no_grad():
+                            inputs = Variable(inputs.cuda())
+                            labels = Variable(labels.cuda())
 
-                    # Wrap them in Variables
-                    # NOTE: A variable forms a thin wrapper around a tensor object, its gradients,
-                    # and a reference to the function that created it.
-                    # The loss function should give a scalar value --> Optimiser will use scalar value and determine
-                    # next epoch's ideal weights
-                    # print('labels pre', labels.shape)
-                    inputs = Variable(inputs.cuda())
-                    labels = Variable(labels.cuda())
+                            # zero the parameter gradients --> Why? Back propagation accumulates gradients, and you don't want
+                            # to mix up gradients between mini batches
+                            optimizer.zero_grad()
+                            # Forward propagation (find output)
+                            # When you feed the images into the model, it will yield a probability of the classification
+                            outputs = model(inputs)
+                            # Find the average of the probability of the classification
+                            # We comment it out because we need tensor for torch.max operation
+                            outputs = torch.mean(outputs)
+                            # Calculate the LOSS (Error) of the classification
+                            # Creates a criterion that measures the mean absolute error (MAE) between each element in
+                            # the output and target (labels) based on whether it is for train or validation
+                            loss = criterion(outputs, labels, phase)
+                            running_loss += loss
+                    else:
+                        # Wrap them in Variables
+                        # NOTE: A variable forms a thin wrapper around a tensor object, its gradients,
+                        # and a reference to the function that created it.
+                        # The loss function should give a scalar value --> Optimiser will use scalar value and determine
+                        # next epoch's ideal weights
+                        # print('labels pre', labels.shape)
+                        inputs = Variable(inputs.cuda())
+                        labels = Variable(labels.cuda())
 
-                    # zero the parameter gradients --> Why? Back propagation accumulates gradients, and you don't want
-                    # to mix up gradients between mini batches
-                    optimizer.zero_grad()
-
-                    # Forward propagation (find output)
-                    # When you feed the images into the model, it will yield a probability of the classification
-                    outputs = model(inputs)
-                    # Find the average of the probability of the classification
-                    # We comment it out because we need tensor for torch.max operation
-                    outputs = torch.mean(outputs)
-                    # Calculate the LOSS (Error) of the classification
-                    # Creates a criterion that measures the mean absolute error (MAE) between each element in
-                    # the output and target (labels) based on whether it is for train or validation
-                    loss = criterion(outputs, labels, phase)
-                    running_loss += loss
+                        # zero the parameter gradients --> Why? Back propagation accumulates gradients, and you don't want
+                        # to mix up gradients between mini batches
+                        optimizer.zero_grad()
+                        # Forward propagation (find output)
+                        # When you feed the images into the model, it will yield a probability of the classification
+                        outputs = model(inputs)
+                        # Find the average of the probability of the classification
+                        # We comment it out because we need tensor for torch.max operation
+                        outputs = torch.mean(outputs)
+                        # Calculate the LOSS (Error) of the classification
+                        # Creates a criterion that measures the mean absolute error (MAE) between each element in
+                        # the output and target (labels) based on whether it is for train or validation
+                        loss = criterion(outputs, labels, phase)
+                        running_loss += loss
 
                     # Why do we back propagate here? We want to recreate the image to determine the spatial frequency
                     # features
@@ -113,6 +131,8 @@ def train_model(model, criterion, optimizer, dataloaders, scheduler,
                     if phase == 'train':
                         loss.sum().backward()
                         optimizer.step()
+
+
 
                     # NOTE: Variable is a wrapper and has multiple components --> We only need to access the data component
                     # Use .detach to access the data for Variables