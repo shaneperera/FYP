4660bbdfa1c290dbe13e83e32478f997e5837dac
shaneperera <42594100+shaneperera@users.noreply.github.com>
2019-08-16 17:34:21 +1000

    Update utils.py

--- utils.py
+++ utils.py
@@ -38,9 +38,10 @@ def plot_training(costs, accs):
 
 def n_p(x):
     """convert numpy float to Variable tensor float"""
-    return Variable(torch.cuda.FloatTensor([x]), requires_grad=False)
+    return Variable(torch.FloatTensor([x]), requires_grad=False)
 
 
+# Calculate number of normal/abnormal images for entire category (eg. XR_WRIST)
 def get_count(df, cat):
     """
     Returns number of images in a study type dataframe which are of abnormal or normal
@@ -48,8 +49,12 @@ def get_count(df, cat):
     df -- dataframe
     cat -- category, "positive" for abnormal and "negative" for normal
     """
-    # By looking at the path column in the pandas dataframe --> Check for the keywords (positive or negative)
-    return df[df['Path'].str.contains(cat)]['Count'].sum()
+    # Check if 'positive or negative' in path --> returns 1 or 0
+    # Previously when iterating over studies, you'd count every picture in that study
+    # Hence, you would do df[df['Path'].str.contains(cat)]['Count'].sum()
+
+    # Don't need to enclose over df[] because we are not calling ['Count']
+    return df['Path'].str.contains(cat).sum()
 
 
 if __name__ == 'main':
